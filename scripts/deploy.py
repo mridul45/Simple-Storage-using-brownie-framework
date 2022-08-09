0ae15b48a9c43d0009c0033f5611d5ee8a49c800
Mridul Singhal <73380291+mridul45@users.noreply.github.com>
2022-08-09 16:22:47 +0530

    Add files via upload

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,9 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 
 def deploy_simple_storage():
 
-    account = accounts[0]
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
     stored_value = simple_storage.retrieve()
 
@@ -14,6 +14,14 @@ def deploy_simple_storage():
     updated_stored_value = simple_storage.retrieve()
 
     print(updated_stored_value)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
